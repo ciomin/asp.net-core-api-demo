c333af43eee1bbc29c4c3833de837ca055961d2b
ciomin <iacobutcosmin@gmail.com>
2024-04-30 22:39:18 +0300

    Add genre listing endpoint with DTO mapping

---
+++ GameStore.Api/Mapping/GenreMapping.cs
@@ -0,0 +1,9 @@
+﻿using GameStore.Api.Dtos;
+using GameStore.Api.Entities;
+
+namespace GameStore.Api.Mapping;
+
+public static class GenreMapping
+{
+    public static GenreDto ToGenreDto(this Genre genre) => new GenreDto(genre.Id, genre.Name);
+}
